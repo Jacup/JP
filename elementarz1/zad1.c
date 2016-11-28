23ce56e2a431c3be7444d4cc0b65a3bfb8c30668
Jacup <jacubgr@gmail.com>
2016-11-28 22:26:50 +0100

    zad1 update

---
+++ elementarz1/zad1.c
@@ -0,0 +1,15 @@
+/* 1. Napisz program obliczający wartość każdego z poniższych wyrażeń:
+
+n1=5+3*8/2-3
+n2=2%2+2*2-2/2
+n3=2*4*(5+9/2) */
+
+#include <stdio.h>
+int main()
+{
+  printf("n1 wynosi : %d\n", 5 + 3 * 8 / 2 - 3);
+  printf("n2 wynosi : %d\n", 2 % 2 + 2 * 2 - 2 / 2);
+  printf("n3 wynosi : %d\n", 2 * 4 * (5 + 9 / 2));
+
+return 0;
+}
