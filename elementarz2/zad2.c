c22eee40f1b50f1c48bd4461bcb482c008727cdb
Jacup <jacubgr@gmail.com>
2016-11-30 15:11:31 +0100

    small changes

--- elementarz2/zad2.c
+++ elementarz2/zad2.c
@@ -15,35 +15,9 @@ int main ()
     if ( n % i == 0 )
     {
       printf("Liczba %d jest liczba zlozona.\n", n);
-      return 1;
+      return 1; 
     }
   }
   printf("Liczba %d jest liczba pierwsza\n", n );
-
-
   return 0;
 }
-
-
-
-
-
-
-
-/*
-#include <stdio.h>
-int main() {
-int n;
-printf("Podaj liczbe calkowita: ");
-scanf("%d\n", &n);
-if (n == 0 || n == 1 )
-printf("ERROR! Liczba %d nie jest ani pierwsza, ani zlozona\n", n);
-else if ( n % 2 == 0 && n % 3 == 0)
-printf("Liczba %d jest liczba zlozona.\n", n);
-else if ( n == 2 || n % 2 != 0  )
-printf("Liczba %d jest liczba pierwsza\n", n);
-
-
-return 0;
-}
-*/
